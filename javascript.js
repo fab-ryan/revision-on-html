67f14b0571833d7042ada73347601be00df5c0b0
fab-ryan <royalfabrice1234@gmail.com>
2022-10-17 11:22:31 +0200

    javascript

---
+++ javascript.js
@@ -0,0 +1,77 @@
+// let number2 = 3;
+// let number = 2;
+// let name = 'hello,world';
+
+// console.log(name.split(',').join(''))
+// console.log(name.includes(','));
+
+// let c = true;
+// let d = [1, 2, 3, 4, 5];
+// while do while for foreach  try catch  if
+// array methods push pop pull length indexOf
+// const indexs=[]
+//     for(let i=1;i<=d.length;i++){
+//         indexs.push(d.indexOf(i))
+//     }
+//     d.forEach((t,z)=>{
+// console.log(t,'itemz',z, 'index')
+//     })
+
+// console.log(indexs)
+
+// let e = {
+//   name: 'John',
+//   age: 30,
+//   func: () => {
+//     return number + number2;
+//   },
+// };
+// console.log(Object.getPrototypeOf(e.func()));
+
+// let f = undefined;
+// let g = null;
+// var h = 1;
+// data types string boolen integer float array object, undefined, null
+// object and array manipulation
+
+// function in javascript
+
+// function adding(z, y) {
+//   console.log(z + y);
+// }
+// const sub = (y,w,s) => {
+//   console.log(y+w+s);
+// };
+// sub(3,4,6);
+
+// adding(100 ,20,9009,9090909);
+// desctruct array and abject
+// const arr = [1, 2, 3, 4, 5];
+// const arr2 = [...arr, 6, 7, 8, 9, 10];
+// console.log(arr2.pop(10));
+// arr2.map((item, index) => {
+//   return (div.innerHTML += `
+//       ${item}
+//       console.log('item' ,item, "index",index)
+//     `);
+// });
+// if params 100
+// function counting those params
+// function divi([],{},number,string){
+//     if(number){
+//         return number
+//     }
+// }
+// divi(,dklkds,dklkd,,dklskd,)
+// class Person {
+   
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     sayHi() {
+//         let name=this.name
+//         console.log(name);
+//     }
+// }
+console.log(Person("hello", 20));
